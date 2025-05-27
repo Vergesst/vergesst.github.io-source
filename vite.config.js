7c9d8939ba1872b605ea15674c1f71c13540b5be
Vergesst <3224171967@qq.com>
2025-05-28 00:38:59 +0800

    update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: process.env.NODE_ENV === 'production'
+    ? '/your-repo-name/' // 替换为你的仓库名称，例如 '/my-vue-project/'
+    : '/'
 })
